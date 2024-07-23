6286dde07891c57998dbc6faef1bf367b7ac3e0a
root <karibay.91@mail.ru>
2024-07-23 05:09:15 +0000

    server saves 23.07

--- notification_system.py
+++ notification_system.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import requests
 import datetime
-
+from loguru import logger
 
 def main():
+    logger.info("Trying to call")
     df = pd.read_excel('data/reviews.xlsx')
 
     addresses = ['Мәңгілік Ел 37', 'Мәңгілік Ел 40', 'Мухамедханова', 'Таха Хусейна 2/1', 'Тәуелсіздік 34']
@@ -23,7 +24,7 @@ def main():
     if text:
         payload = {
             'public_key': '22dc3a76017437d85401e95423154821',
-            'phone': '+77088504506',
+            'phone': '+77011994199',
             'campaign_id': '1124317061',
             'text': text
         }
@@ -32,14 +33,15 @@ def main():
 
         print(response.status_code)
         print(response.json())
+        logger.success("Bot has called!")
 
 
 if __name__ == '__main__':
     import schedule
     import time
 
-    # Schedule the function to run every day at 14:00
-    schedule.every().day.at("14:55").do(main)
+    # Schedule the function to run every day at 07:30
+    schedule.every().day.at("02:30").do(main)
 
     # Infinite loop to continuously check and run scheduled tasks
     while True:
